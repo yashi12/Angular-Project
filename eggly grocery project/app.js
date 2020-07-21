367cd95346a1b62c0db8f2e61ddeea9691aed92d
yashi12 <yashiagarwal1812@gmail.com>
2020-07-21 18:10:18 +0530

    Fix ui-router

--- eggly grocery project/app.js
+++ eggly grocery project/app.js
@@ -1,23 +1,22 @@
+const GroceryListItemController = function ($scope) {
 
-const GroceryListItemController = function($scope){
-  
 
     $scope.categorieItems = [
-        {name:'breakfast'},
-        {name:'lunch'},
-        {name:'dinner'}
+        {name: 'breakfast'},
+        {name: 'lunch'},
+        {name: 'dinner'}
     ]
     $scope.groceryItems = [
-        {id:1,completed:true , itemName:'milk', date:'2020-01-01',category:'breakfast'},
-        {id:2,completed:true , itemName:'cookies', date:'2020-02-01',category: 'breakfast'},
-        {id:3,completed:true , itemName:'butter', date:'2020-03-01',category: 'lunch'},
-        {id:4,completed:true , itemName:'bread', date:'2020-01-07',category: 'dinner'},
-        {id:5,completed:true , itemName:'cheese', date:'2020-02-11',category: 'lunch'},
-        {id:6,completed:true , itemName:'eggs', date:'2020-08-01',category: 'dinner'}
+        {id: 1, completed: true, itemName: 'milk', date: '2020-01-01', category: 'breakfast'},
+        {id: 2, completed: true, itemName: 'cookies', date: '2020-02-01', category: 'breakfast'},
+        {id: 3, completed: true, itemName: 'butter', date: '2020-03-01', category: 'lunch'},
+        {id: 4, completed: true, itemName: 'bread', date: '2020-01-07', category: 'dinner'},
+        {id: 5, completed: true, itemName: 'cheese', date: '2020-02-11', category: 'lunch'},
+        {id: 6, completed: true, itemName: 'eggs', date: '2020-08-01', category: 'dinner'}
     ]
 
     $scope.currentCategory = null;
-    var SetCurrentCategory = function(category){
+    let SetCurrentCategory = function (category) {
         $scope.currentCategory = category;
         CancelCreating();
         CancelEditing();
@@ -25,8 +24,8 @@ const GroceryListItemController = function($scope){
         // debug
         console.log($scope.currentCategory);
     }
-    var IsCurrentCategory = function(category)  {
-        return $scope.currentCategory!=null && category.name === $scope.currentCategory.name;
+    let IsCurrentCategory = function (category) {
+        return $scope.currentCategory != null && category.name === $scope.currentCategory.name;
     }
 
     $scope.SetCurrentCategory = SetCurrentCategory;
@@ -37,19 +36,19 @@ const GroceryListItemController = function($scope){
 //    CRUD
 //    -----------------------------------------------------------------
 
-    var resetCreateForm  =function(){
+    let resetCreateForm = function () {
         $scope.newItem = {
-            completed:true,
-            itemName:'',
-            date:'',
-            category:$scope.currentCategory.name
+            completed: true,
+            itemName: '',
+            date: '',
+            category: $scope.currentCategory.name
         }
     }
-    var createItem = function(newItem){
-        let maxItem = _.max($scope.groceryItems,function (item) {
+    let createItem = function (newItem) {
+        let maxItem = _.max($scope.groceryItems, function (item) {
             return item.id;
         });
-        newItem.id = maxItem.id+1;
+        newItem.id = maxItem.id + 1;
         // newItem.id = $scope.groceryItems.length+1;
         newItem.date = new Date();
         $scope.groceryItems.push(newItem);
@@ -59,30 +58,29 @@ const GroceryListItemController = function($scope){
 
     $scope.currentItem = null;
 
-    var setEditItem = function(item){
+    let setEditItem = function (item) {
         $scope.currentItem = angular.copy(item);
     }
-    var updateItem = function(currentItem){
-        let index = _.findIndex($scope.groceryItems,function (item) {
+    let updateItem = function (currentItem) {
+        let index = _.findIndex($scope.groceryItems, function (item) {
             return item.id == currentItem.id;
         })
         $scope.groceryItems[index] = currentItem;
         $scope.isEditing = false;
         $scope.currentItem = null;
     }
-    var isSelecteditem = function(itemId){
-        return $scope.currentItem !=null && $scope.currentItem.id == itemId;
+    let isSelecteditem = function (itemId) {
+        return $scope.currentItem != null && $scope.currentItem.id == itemId;
     }
-    var deleteItem = function(currentItem){
-        let index = _.findIndex($scope.groceryItems,function (item) {
+    let deleteItem = function (currentItem) {
+        let index = _.findIndex($scope.groceryItems, function (item) {
             return item.id == currentItem.id;
         })
-        $scope.groceryItems.splice(index,1);
+        $scope.groceryItems.splice(index, 1);
         // _.remove($scope.groceryItems, function (item) {
         //     return item.id == currentItem.id;
         // });
     }
-
 
 
     $scope.createItem = createItem;
@@ -95,32 +93,32 @@ const GroceryListItemController = function($scope){
 //    ----------------------------------------------------------------
 //    CREATING AND EDITING STATES
 //    -----------------------------------------------------------------
-    $scope.isCreating=false;
+    $scope.isCreating = false;
     $scope.isEditing = false;
 
-    var StartCreating = function(){
-        $scope.isCreating=true;
+    let StartCreating = function () {
+        $scope.isCreating = true;
         $scope.isEditing = false;
 
         resetCreateForm();
     }
-    var CancelCreating =function(){
-        $scope.isCreating=false;
+    let CancelCreating = function () {
+        $scope.isCreating = false;
     }
-    var StartEditing = function(){
-        $scope.isCreating=false;
+    let StartEditing = function () {
+        $scope.isCreating = false;
         $scope.isEditing = true;
     }
-    var CancelEditing =function(){
-        $scope.isEditing=false;
+    let CancelEditing = function () {
+        $scope.isEditing = false;
     }
-    var ShouldShowCreating=function(){
+    let ShouldShowCreating = function () {
         console.log("currentcategory  " + $scope.currentCategory);
         console.log(!$scope.isEditing);
         console.log($scope.currentCategory && !$scope.isEditing);
         return $scope.currentCategory && !$scope.isEditing;
     }
-    var ShouldShowEditing=function(){
+    let ShouldShowEditing = function () {
         return !$scope.isCreating && $scope.isEditing;
     }
     $scope.StartCreating = StartCreating;
@@ -131,56 +129,36 @@ const GroceryListItemController = function($scope){
     $scope.ShouldShowEditing = ShouldShowEditing;
 };
 
-const router = function($routeProvider) {
+const router = function ($routeProvider) {
     console.log("app.js config launched");
     $routeProvider
-        .when('/',{
-            templateUrl:'view/groceryList.html'
+        .when('/', {
+            templateUrl: 'view/groceryList.html'
             // controller:'GroceryListItemController'
         })
-        .when('/addItem',{
-            templateUrl:'view/inputItem.html'
+        .when('/addItem', {
+            templateUrl: 'view/inputItem.html'
             // controller:'GroceryListItemController'
         })
         .otherwise({
-            templateUrl:'https://docs.angularjs.org/api/ngRoute/directive/ngView'
+            templateUrl: 'https://docs.angularjs.org/api/ngRoute/directive/ngView'
             // redirectTo:'/'
         });
 }
 
 
-
 angular
-    // .module('groceryList',['ui.router'])
-    .module('groceryList',['ui.router','categories','categories.items'])
-    .controller("GroceryListItemController",GroceryListItemController)
-    // .config(function($stateProvider,$urlRouterProvider){
-    //     console.log("app.js");
-    //     $stateProvider
-    //     .state('eggly',{
-    //         url:'',
-    //         templateUrl:'<ui-view/>',
-    //        abstract:true
-    //     });
-    //     console.log("end");
-    //     $urlRouterProvider.otherwise('');
-    // })
-    .config(['$stateProvider','$urlRouterProvider',function ($stateProvider,$urlRouterProvider) {
+    // .module('groceryList', ['ui.router','categories'])
+    .module('groceryList', ['ui.router', 'categories', 'categories.items'])
+    .controller("GroceryListItemController", GroceryListItemController)
+    .config(function ($stateProvider, $urlRouterProvider) {
         console.log("app.js");
         $stateProvider
-            .state('eggly',{
-                url:'',
-                views:{
-                    'categories':{
-                        // template:'<h1>hello</h1>',
-                        controller:'GroceryListItemController',
-                        templateUrl:'categories/categories.tmpl.html'
-                    },
-                    'items':{
-                        controller:'GroceryListItemController',
-                        templateUrl:'categories/items/items.tmpl.html'
-                    }
-                }
-            })
+            .state('eggly', {
+                url: '',
+                template: '<ui-view/>',
+                abstract: true
+            });
         console.log("end");
-    }])
+        $urlRouterProvider.otherwise('');
+    })
--- eggly grocery project/categories/categories.js
+++ eggly grocery project/categories/categories.js
@@ -1,29 +1,26 @@
-angular.module('categories',[
-    'eggly.models.categories'
-])
-    // .config(function($stateProvider){
-    //     console.log("categories.js");
-    //     $stateProvider
-    //     .state('eggly.categories',{
-    //         url:'',
-    //         views:{
-    //             'categories@':{
-    //                 // template:'<h1>hello</h1>',
-    //                 controller:'CategoriesCtrl',
-    //                 templateUrl:'categories/categories.tmpl.html'
-    //             },
-    //             'items@':{
-    //                 controller:'ItemsCtrl',
-    //                 templateUrl:'categories/items/items.tmpl.html'
-    //             }
-    //         }
-    //     })
-    //     .controller('CategoriesCtrl',function CategoriesCtrl($scope){
-    //
-    //     })
-    //     .controller('ItemsCtrl',function ($scope) {
-    //
-    //     });
-    //
-    //     console.log("end categories.js");
-    // });
+const categoriesApp = angular.module('categories', ['ui.router']);
+categoriesApp.config(function ($stateProvider) {
+    console.log("categories.js");
+    $stateProvider
+        .state('eggly.categories', {
+            url: '/',
+            views: {
+                'categories@': {
+                    controller: 'CategoriesCtrl',
+                    templateUrl: 'categories/categories.tmpl.html'
+                },
+                'items@': {
+                    controller: 'ItemsCtrl',
+                    templateUrl: 'categories/items/items.tmpl.html'
+                }
+            }
+        })
+    console.log("end categories.js");
+})
+categoriesApp.controller('CategoriesCtrl', function CategoriesCtrl($scope) {
+
+});
+
+categoriesApp.controller('ItemsCtrl', function ItemsCtrl($scope) {
+
+});
