339337356323071f162b65797a2e6c58ff4524a9
yashi12 <yashiagarwal1812@gmail.com>
2020-07-22 14:43:28 +0530

    fix(bug) : edit item
    - if category selected item will edit

--- eggly grocery project/categories/items/edit/items-edit.js
+++ eggly grocery project/categories/items/edit/items-edit.js
@@ -1,33 +1,40 @@
 angular.module('categories.items.edit', [])
     .config(function ($stateProvider) {
+        console.log("edit");
         $stateProvider
-            .state('eggly.categories.items.edit',{
-                url:'/items/edit',
-                templateUrl:'categories/items/edit/items-edit-tmpl.html',
-                controller:'EditItemCtrl as editItemCtrl'
+            .state('eggly.categories.items.edit', {
+                url: '/items/edit/:id',
+                templateUrl: 'categories/items/edit/items-edit-tmpl.html',
+                controller: 'EditItemCtrl as editItemCtrl'
             })
     })
-    .controller('EditItemCtrl' ,function ($state,$stateParams,ItemsModel) {
-        let editItemCtrl=this;
+    .controller('EditItemCtrl', function ($state, $stateParams, ItemsModel) {
+        // console.log("stateParams");
+        // console.log($stateParams);
+        let editItemCtrl = this;
+
         function returnToItems() {
-            $state.go('eggly.categories.items',{
-                category:$stateParams.category
+            $state.go('eggly.categories.items', {
+                category: $stateParams.category
             })
         }
+
         function CancelEditing() {
             return returnToItems();
         }
-        function updateItem(){
+
+        function updateItem() {
             editItemCtrl.item = angular.copy(editItemCtrl.editedItem);
             ItemsModel.updateItem(editItemCtrl.item);
             returnToItems();
         }
+
         ItemsModel.getItemById($stateParams.id)
             .then(function (item) {
-                if(item){
+                if (item) {
                     editItemCtrl.item = item;
-                    editItemCtrl.editedItem  = angular.copy(editItemCtrl.item);
-                }else {
+                    editItemCtrl.editedItem = angular.copy(editItemCtrl.item);
+                } else {
                     returnToItems();
                 }
             })
