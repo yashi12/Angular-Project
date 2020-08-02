046641967f5ff18787d55c2af8a0ff38b2e3398f
yashi12 <yashiagarwal1812@gmail.com>
2020-08-02 23:37:55 +0530

    Cleanup for output management

--- npm projects/test/src/index.js
+++ npm projects/test/src/index.js
@@ -1,21 +1,9 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.jpg';
-import Data from './data.xml';
 
 function component() {
     const element = document.createElement('div');
 
-    // Lodash, currently included via this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    const myIcon = new Image();
-    myIcon.src = Icon;
-
-    element.appendChild(myIcon);
-
-    console.log(Data);
 
     return element;
 }
